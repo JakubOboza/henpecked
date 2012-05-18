4bfbd4fb750442f7529312e83ce93a7c9cf41839
JakubOboza <jakub.oboza@gmail.com>
2012-05-18 12:06:14 +0100

    added few command classes

--- lib/henpecked/base.rb
+++ lib/henpecked/base.rb
@@ -44,6 +44,52 @@ module Henpecked
 
     end # end of AcknowledgeHostProblem class
 
+    class AcknowledgeHostProblemExpire < AbstractAlert
+
+      def initialize(host_name = nil, sticky = nil, notify = nil, persistent = nil, timestamp = nil, author = nil, comment = nil)
+        @host_name = host_name
+        @sticky = sticky
+        @notify = notify
+        @persistent = persistent
+        @timestamp = timestamp
+        @author = author
+        @comment = comment
+      end    
+
+      def arguments
+        [@host_name, @sticky, @notify, @persistent, @timestamp, @author, @comment].join(";")
+      end
+
+    end # end of AcknowledgeHostProblemExpire class
+      
+    class DisableHostCheck < AbstractAlert
+
+      def initialize(host_name = nil)
+        @host_name = host_name
+      end
+
+      def arguments
+        @host_name
+      end
+
+    end # end of DisableHostCheck class
+
+    class SendCustomHostNotification < AbstractAlert
+
+      def initialize(host_name = nil, options = nil, author = nil, comment = nil)
+        @host_name = host_name
+        @options = options 
+        @author = author
+        @comment = comment
+      end
+
+      def arguments
+        [@host_name, @options, @author, @comment].join(";")
+      end
+
+    end # end of SendCustomHostNotificatio class
+    
+
   end
 
 end
