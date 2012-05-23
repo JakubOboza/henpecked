b869efabcd803a8bdd509b1d91eb878674ba36fc
JakubOboza <jakub.oboza@gmail.com>
2012-05-23 12:15:56 +0100

    added test for pushing

--- spec/push_spec.rb
+++ spec/push_spec.rb
@@ -4,16 +4,25 @@ describe Henpecked do
 
   describe "#push" do
 
-    it "should be able to push alert" do
+    before(:each) do
       test_path = "/tmp/henpeck.test"
       File.open(test_path, "w")
-      lambda do
-        Henpecked.config do
-          cmd_file test_path
-        end
-        Henpecked.push(Henpecked::Alert::AcknowledgeHostProblem.new)
+      Henpecked.config do
+        cmd_file test_path
+      end
+    end
 
+    it "should be able to push alert" do      
+      lambda do
+        Henpecked.push(Henpecked::Alert::AcknowledgeHostProblem.new)
       end.should_not raise_error
+    end
+
+    it "should append to a file" do
+      Henpecked.push(Henpecked::Alert::AcknowledgeHostProblem.new)
+      Henpecked.push(Henpecked::Alert::AcknowledgeHostProblem.new)        
+      Henpecked.push(Henpecked::Alert::AcknowledgeHostProblem.new)
+      File.foreach("/tmp/henpeck.test").inject(0) {|c, line| c+1}.should eql(2)
     end
 
   end
