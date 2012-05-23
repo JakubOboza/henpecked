529b9cb7c2f50658f0e99da2ca2cd3e64be2d5e2
JakubOboza <jakub.oboza@gmail.com>
2012-05-23 11:49:33 +0100

    changed write mode to a

--- lib/henpecked/push.rb
+++ lib/henpecked/push.rb
@@ -1,10 +1,10 @@
 module Henpecked
 
   def self.push(alert)
-  	raise Exception.new "Alert object has to have icinga_description method" unless alert.respond_to?(:icinga_description)
-  	File.open(Henpecked.file_path, "w") do |file|  
-      file.write(alert.icinga_description + "\n")
-  	end
+    raise Exception.new "Alert object has to have icinga_description method" unless alert.respond_to?(:icinga_description)
+    File.open(Henpecked.file_path, "a") do |file|
+    file.write(alert.icinga_description + "\n")
+    end
   end
 
 end
